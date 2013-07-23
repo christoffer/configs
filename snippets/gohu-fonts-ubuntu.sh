149ba443a842baa505d14f04269220da76619095
Christoffer Klang <christoffer.klang@gmail.com>
2013-07-23 12:01:31 +0200

    Add cleaner output from gohu script

--- snippets/gohu-fonts-ubuntu.sh
+++ snippets/gohu-fonts-ubuntu.sh
@@ -9,16 +9,20 @@ fi
 work_dir=/tmp/get-gohu
 mkdir -p $work_dir
 cd $work_dir
-wget http://font.gohu.org/gohufont-2.0.tar.gz
-tar xvzf gohufont-2.0.tar.gz
+echo "Downloading font"
+wget -q http://font.gohu.org/gohufont-2.0.tar.gz
+echo "Unpacking"
+tar xzf gohufont-2.0.tar.gz
 cd gohufont-2.0
 gunzip -f *.gz
 
 mkdir -p $HOME/.fonts
 
+echo "Moving into place"
 mv $work_dir/gohufont-2.0/*.pcf $HOME/.fonts
-fc-cache -f -v
+fc-cache -f
 
+echo "Enabling bitmap fonts"
 blocker=/etc/fonts/conf.d/70-no-bitmaps.conf
 if [ -e $blocker ] || [ -L $blocker ] ; then
   rm $blocker
@@ -28,5 +32,8 @@ enabler=/etc/fonts/conf.avail/70-force-bitmaps.conf
 if [ ! -e $enabler ]; then
   ln -s $enabler /etc/fonts/conf.d
 fi
+
+echo "Cleaning up"
+rm -rf "$work_dir"
 
 echo "All should be good now, please restart X"
